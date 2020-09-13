56e3185d459108ac86e17254f78ca4c5d7c98ada
Kapil Singh <KapilSingh@msn.com>
2020-09-13 16:53:56 -0400

    env2

--- env2/Scripts/Activate.ps1
+++ env2/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Kapil\Source\Repos\IsoDataScheduler\env2"
+$env:VIRTUAL_ENV="C:\Users\Kapil\Source\Repos\IsoDataScheduler\env3"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
@@ -35,7 +35,7 @@ if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     function global:_OLD_VIRTUAL_PROMPT {""}
     copy-item function:prompt function:_OLD_VIRTUAL_PROMPT
     function global:prompt {
-        Write-Host -NoNewline -ForegroundColor Green '(env2) '
+        Write-Host -NoNewline -ForegroundColor Green '(env3) '
         _OLD_VIRTUAL_PROMPT
     }
 }
