1e56c330ef0197d279428b13e1bac18c7316ee98
Kapil Singh <KapilSingh@msn.com>
2021-03-25 20:15:13 -0400

    minor all

--- isoDataScheduler.py
+++ isoDataScheduler.py
@@ -14,23 +14,25 @@ import matplotlib
 from datetime import datetime
 from pytz import timezone
 from GridCPShaving import GridCPShaving
-
+from datetime import datetime, timedelta
 def main():
     def putIsoData(dataMiner, isoHelper):
-
+        print("1")
         dataMiner.fetch_LMP(1, isoHelper)
         dataMiner.fetch_InstantaneousLoad(1, 'ps',isoHelper)
         dataMiner.fetch_InstantaneousLoad(1, 'PJM RTO',isoHelper)
-
+        print("2")
         dataMiner.fetch_GenFuel(11, isoHelper)
+        print("3")
         #dataMiner.fetch_hourlyMeteredLoad(True, 'CurrentYear', True,isoHelper)
         #dataMiner.fetch_hourlyMeteredLoad(False, 'CurrentYear', True,isoHelper)
         meterData.fetchMeterData('550001081', 1, isoHelper)
         #dataMiner.fetch_7dayLoadForecast(True, isoHelper)
+        print("4")
         dataMiner.fetch_LoadForecast( 'ps', isoHelper,GCPShave)
         #dataMiner.fetch_7dayLoadForecast(False, isoHelper)
         dataMiner.fetch_LoadForecast('PJM RTO', isoHelper,GCPShave)
-
+        print("5")
         df = isoHelper.getLmp_latest(nodeId='PSEG',numIntervals=6)
 
       
@@ -47,8 +49,7 @@ def main():
     
     eastern = timezone('US/Eastern')
 
-    oldestTimeStamp = datetime(2021,3,14)
-
+    oldestTimeStamp =  datetime.now() - timedelta (hours =-1)
     meterData.fetchMeterData('550001081', 1000, isoHelper)
     currentDate =datetime.today();
     #startMeteredPeriod =  datetime(currentDate.year-1, currentDate.month, 1)
@@ -68,8 +69,8 @@ def main():
 
     
     dataMiner.fetch_LMP(8640, isoHelper)
-    dataMiner.fetch_InstantaneousLoad(8640, 'ps',isoHelper)
-    dataMiner.fetch_InstantaneousLoad(8640, 'PJM RTO',isoHelper)
+    dataMiner.fetch_InstantaneousLoad(4320, 'ps',isoHelper)
+    dataMiner.fetch_InstantaneousLoad(4320, 'PJM RTO',isoHelper)
 
     #dataMiner.fetch_GenFuel(528, isoHelper)
         #GCPShave.findPeaks(oldestTimeStamp, 'ps', False, False, isoHelper)
