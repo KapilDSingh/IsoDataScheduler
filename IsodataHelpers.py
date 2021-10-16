36993dd247c43e3d6ddd5de92616216ec49de66c
Kapil Singh <KapilSingh@msn.com>
2021-10-16 15:23:53 -0400

    Changed IP of contactor relay status

--- IsodataHelpers.py
+++ IsodataHelpers.py
@@ -493,10 +493,12 @@ class IsodataHelpers(object):
             return results
 
     def SetRelayState(self, state):
+
+       
         
         relaySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        relaySocket.connect(('68.195.11.115', 144))
+        relaySocket.connect(('68.195.10.119', 144))
         print(relaySocket)
 
 
@@ -510,10 +512,10 @@ class IsodataHelpers(object):
 
 
     def  GettRelayState(self):
-       
+    
         relaySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        relaySocket.connect(('68.195.11.115', 144))
+        relaySocket.connect(('68.195.10.119', 144))
 
         print(relaySocket)
         stateStr = "GET /stateFull.xml?noReply=0 HTTP/1.1\r\nAuthorization: Basic bm9uZTp3ZWJyZWxheQ=\r\n\r\n=" 
