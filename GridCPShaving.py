426178d9e1d674787830fc2ba2d504060d33db54
KapilDSingh <kapilsingh@msn.com>
2023-07-09 16:14:28 -0400

    Bug

--- GridCPShaving.py
+++ GridCPShaving.py
@@ -140,7 +140,7 @@ class GridCPShaving(object):
 
             if (len(peakDf) == 1):
                 peakStartTime = peakDf[0].timestamp  + timedelta(hours =-1)
-                sql_query = "into into peakSignalTbl Values (" + peakDf[0].timestamp, peakDf[0].Area, peakDf[0].Peak, peakDf[0].EvaluatedAt, peakStartTime  +")"
+                sql_query = "insert into peakSignalTbl Values (" + peakDf[0].timestamp, peakDf[0].Area, peakDf[0].Peak, peakDf[0].EvaluatedAt, peakStartTime  +")"
                 result = connection.execute(sql_query)
 
                 if  ((peakStartTime <= peakDf[0].EvaluatedAt) and (peakDf[0].EvaluatedAt <= peakDf[0].timestamp)):
